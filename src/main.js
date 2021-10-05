2536c856671da0a1b5a8b41e6d1f2dfc7611977a
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2021-10-05 13:43:44 -0400

    update filters

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import filter from './filters'
 import Vuelidate from 'vuelidate'
 import money from 'v-money'
 import VModal from 'vue-js-modal'
@@ -15,5 +16,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  filter,
   render: h => h(App)
 }).$mount('#app')
